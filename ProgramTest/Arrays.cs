fe86e0352c35dcd0f3265987a349703c54eb95b1
vgmda <vasilegancin@gmail.com>
2022-05-02 16:46:39 +0100

    New file Arrays.cs

---
+++ ProgramTest/Arrays.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ProgramTest
+{
+    class Arrays
+    {
+
+        private static void Main(string[] args)
+        {
+            int[] luckyNumbers = { 1, 2, 3, 4 };
+
+            Console.WriteLine(luckyNumbers[2]);
+
+
+        }
+
+    }
+}
+
