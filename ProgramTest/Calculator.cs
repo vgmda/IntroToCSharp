23c1673e1bbf1d50cb8d6fb0087d595fc242aecd
vgmda <vasilegancin@gmail.com>
2022-05-02 16:04:37 +0100

    Added Calculator.cs

---
+++ ProgramTest/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ProgramTest
+{
+    class Calculator
+    {
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow");
+
+
+
+
+
+        }
+
+    }
+}
+
