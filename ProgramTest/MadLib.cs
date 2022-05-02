1b20064fe0384cffc9fe9e350d1d2c24cb3cac66
vgmda <vasilegancin@gmail.com>
2022-05-02 16:29:21 +0100

    Added MadLib

---
+++ ProgramTest/MadLib.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace ProgramTest
+{
+    class MadLib
+    {
+
+        private static void Main(string[] args)
+        {
+            string color, pluralNoun, celebrity;
+
+
+            Console.WriteLine("Enter a color: ");
+            color = Console.ReadLine();
+
+            Console.WriteLine("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.WriteLine("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I love " + celebrity);
+
+        }
+
+    }
+}
+
