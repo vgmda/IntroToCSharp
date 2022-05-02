f166866bf79ee263d84ca7cb27419d60e0f0564b
vgmda <vasilegancin@gmail.com>
2022-05-02 17:31:05 +0100

    If conditions

--- ProgramTest/IfStatements.cs
+++ ProgramTest/IfStatements.cs
@@ -1,11 +1,42 @@
 ﻿using System;
 namespace ProgramTest
 {
-	public class IfStatements
-	{
-		public IfStatements()
-		{
-		}
-	}
+    class IfStatements
+    {
+
+        private static void Main(string[] args)
+        {
+            bool isFirst = true;
+            bool isWinner = false;
+
+            //if (isFirst && isWinner)
+            //{
+            //    Console.WriteLine("This statement is true");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("This statement is false");
+            //}
+
+            if (isFirst && isWinner)
+            {
+                Console.WriteLine("This statement is true");
+            }
+            else if (isFirst && !isWinner)
+            {
+                Console.WriteLine("This statement is true/false");
+            }
+
+            else
+            {
+                Console.WriteLine("This statement is false");
+            }
+
+
+
+
+        }
+
+    }
 }
 
