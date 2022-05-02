a565a2cbf8911a955f88b4aac01d37cd448c10f3
vgmda <vasilegancin@gmail.com>
2022-05-02 12:30:18 +0100

    Test

--- ProgramTest/Program.cs
+++ ProgramTest/Program.cs
@@ -21,9 +21,9 @@ namespace ProgramTest
 
 
             Console.WriteLine("There once was a man named " + characterName);
-            //Console.WriteLine("He was " + characterAge + " years old");
-            //Console.WriteLine("He really like the name " + characterName);
-            //Console.WriteLine("But didn't like being " + characterAge);
+            Console.WriteLine("He was " + characterAge + " years old");
+            Console.WriteLine("He really like the name " + characterName);
+            Console.WriteLine("But didn't like being " + characterAge);
 
 
 
