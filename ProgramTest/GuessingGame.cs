411a063daaf2cbf81600942c0024f904e4958b43
vgmda <vasilegancin@gmail.com>
2022-05-03 20:32:31 +0100

    Added the GuessingGame

---
+++ ProgramTest/GuessingGame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ProgramTest
+{
+    class GuessingGame
+    {
+
+        private static void Main(string[] args)
+        {
+            string secretWord = "giraffe";
+            string guess = "";
+
+            /*
+            while (guess != secretWord)
+            {
+                Console.Write("Enter guess: ");
+                guess = Console.ReadLine();
+
+            }
+
+            Console.WriteLine("You guessed the secret word!");
+            */
+
+
+        }
+    }
+}
