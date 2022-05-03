f1b13a7e5b5a4c2cfbc1cdd8943dab4e2b0940c3
vgmda <vasilegancin@gmail.com>
2022-05-03 21:54:10 +0100

    Added try & catch methods

--- ProgramTest/ExceptionHandling.cs
+++ ProgramTest/ExceptionHandling.cs
@@ -5,12 +5,20 @@ namespace ProgramTest
     {
         private static void Main(string[] args)
         {
-            Console.Write("Enter a number: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter another number: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.Write("Enter a number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter another number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine(num1 / num2);
+                Console.WriteLine(num1 / num2);
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+            }
+
 
         }
 
