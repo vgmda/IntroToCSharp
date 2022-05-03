63ddbba00e3597b24882661a5eab91e6fcb92701
vgmda <vasilegancin@gmail.com>
2022-05-03 21:49:50 +0100

    Added Exception Handling

---
+++ ProgramTest/ExceptionHandling.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ProgramTest
+{
+    class ExceptionHandling
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1 / num2);
+
+        }
+
+    }
+
+}
