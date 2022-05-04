3f3bc40cebfbeb6475e8f03b4508630bfc0dab36
vgmda <vasilegancin@gmail.com>
2022-05-04 21:59:55 +0100

    Added Student class

---
+++ ProgramTest/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ProgramTest
+{
+    class Student
+    {
+        public string name;
+        public string major;
+        public double gpa;
+
+
+        public Student(string aName, string aMajor, double aGpa)
+        {
+            name = aName;
+            major = aMajor;
+            gpa = aGpa;
+
+        }
+    }
+}
+
