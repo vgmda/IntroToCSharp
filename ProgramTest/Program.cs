6d9b3de548fb2edb98e8fc542818398c35a5b6a1
vgmda <vasilegancin@gmail.com>
2022-05-04 21:32:45 +0100

    Added Book2

--- ProgramTest/Program.cs
+++ ProgramTest/Program.cs
@@ -13,6 +13,11 @@ namespace ProgramTest
             book1.author = "JK Rowling";
             book1.pages = 400;
 
+            Book book2 = new Book();
+            book2.title = "Lord Of the Rings";
+            book2.author = "Tolkein";
+            book2.pages = 700;
+
             Console.WriteLine(book1.title);
 
         }
