f0db7424d6577c183c22554b8b90364262a18c24
vgmda <vasilegancin@gmail.com>
2022-05-04 22:54:53 +0100

    Added songCount counter

--- ProgramTest/Program.cs
+++ ProgramTest/Program.cs
@@ -32,7 +32,9 @@ namespace ProgramTest
             // Console.WriteLine(avengers.Rating);
 
             Song holiday = new Song("Holiday", "Green Day", 200);
+            Console.WriteLine(Song.songCount);
             Song kashmir = new Song("Kashmir", "Led Zeppelin", 150);
+            Console.WriteLine(Song.songCount);
 
         }
 
--- ProgramTest/Song.cs
+++ ProgramTest/Song.cs
@@ -6,12 +6,16 @@ namespace ProgramTest
         public string title;
         public string artist;
         public int duration;
+        //Global variable which can be accessed as a part of a Song class
+        public static int songCount = 0;
 
         public Song(string aTitle, string aArtist, int aDuration)
         {
             title = aTitle;
             artist = aArtist;
             duration = aDuration;
+            //Increment the songCount everytime a new Song() object is created
+            songCount++;
 
         }
     }
