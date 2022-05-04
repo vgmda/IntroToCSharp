794fac8dbbb1d013d488ceb107728d71e516490f
vgmda <vasilegancin@gmail.com>
2022-05-04 22:17:59 +0100

    Added Movie() class

---
+++ ProgramTest/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ProgramTest
+{
+    class Movie
+    {
+        public string title;
+        public string director;
+        private string rating;
+
+
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            rating = aRating;
+        }
+    }
+}
+
