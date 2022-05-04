63daa5c33c44df598913cf3d511c90298aa4430d
vgmda <vasilegancin@gmail.com>
2022-05-04 23:15:45 +0100

    Added Chef() class

---
+++ ProgramTest/Chef.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ProgramTest
+{
+    public class Chef
+    {
+        public void MakeChicken()
+        {
+            Console.WriteLine("The Chef makes chicken");
+        }
+
+        public void MakeSalad()
+        {
+            Console.WriteLine("The Chef makes salad");
+        }
+
+        public virtual void MakeSpecialDish()
+        {
+            Console.WriteLine("The Chef makes bbq ribs");
+        }
+    }
+}
+
