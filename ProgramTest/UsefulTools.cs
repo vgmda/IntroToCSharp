ca1b4efdde0ae38a5855d64939fa361bf47aa759
vgmda <vasilegancin@gmail.com>
2022-05-04 23:05:48 +0100

    Added UsefulTools class

---
+++ ProgramTest/UsefulTools.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ProgramTest
+{
+    public class UsefulTools
+    {
+        public static void SayHi(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+
+
+    }
+}
+
