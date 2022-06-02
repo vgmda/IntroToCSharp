649f447e1de4fcab3bdc3369e4701e5a2389ef50
vgmda <vasilegancin@gmail.com>
2022-06-02 21:24:54 +0100

    Commit

--- ProgramTest/Program.cs
+++ ProgramTest/Program.cs
@@ -21,7 +21,7 @@ namespace ProgramTest
 
             Student student1 = new Student("Jim", "Business", 2.8);
             Student student2 = new Student("Pam", "Art", 3.6);
-
+            // Test
 
             // Console.WriteLine(student1.HasHonors());
             // Console.WriteLine(student2.HasHonors());
